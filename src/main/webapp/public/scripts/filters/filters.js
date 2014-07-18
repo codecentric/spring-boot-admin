0b24e0da480c0334a6d443cb6f40fa245e03fa73
Thomas Bosch <tbosch@gmx.de>
2014-07-18 09:03:31 +0200

    better uptime

--- src/main/webapp/public/scripts/filters/filters.js
+++ src/main/webapp/public/scripts/filters/filters.js
@@ -5,4 +5,16 @@ angular.module('springBootAdmin')
 		return function(input) { 
 			return Math.ceil(input); 
 		} 
+	})
+	.filter('floor', function() { 
+		return function(input) { 
+			return Math.floor(input); 
+		} 
+	})
+	.filter('padZero', function() { 
+		return function(input) { 
+			var s = input + "";
+		    while (s.length < 2) s = "0" + s;
+		    return s;
+		} 
 	});
