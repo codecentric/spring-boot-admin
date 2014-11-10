620779a6f1fc0a52e5c9f3137eb53b3c783841b2
Dennis Schulte <mail@dennis-schulte.de>
2014-11-10 14:17:50 +0100

    Add Integrationtests for the REST API

--- spring-boot-admin-server/src/test/java/de/codecentric/boot/admin/service/ApplicationRegistryTest.java
+++ spring-boot-admin-server/src/test/java/de/codecentric/boot/admin/service/ApplicationRegistryTest.java
@@ -7,13 +7,13 @@ import static org.junit.Assert.assertTrue;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.ContextConfiguration;
+import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import de.codecentric.boot.admin.config.WebappConfig;
+import de.codecentric.boot.admin.TestAdminApplication;
 import de.codecentric.boot.admin.model.Application;
 
-@ContextConfiguration(classes = { WebappConfig.class })
+@SpringApplicationConfiguration(classes=TestAdminApplication.class)
 @RunWith(SpringJUnit4ClassRunner.class)
 public class ApplicationRegistryTest {
 
