ce78421a09ef5b6e6c30ba9f5ad638fe7a4a6ab1
James Burton <the.james.burton@gmail.com>
2015-08-13 14:15:58 +0100

    initial war file sample

---
+++ spring-boot-admin-samples/spring-boot-admin-sample-war/src/main/java/de/codecentric/application/Application.java
@@ -0,0 +1,27 @@
+package de.codecentric.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.context.annotation.Configuration;
+
+import de.codecentric.boot.admin.config.EnableAdminServer;
+
+@Configuration
+@EnableAutoConfiguration
+@EnableAdminServer
+@SpringBootApplication
+public class Application extends SpringBootServletInitializer {
+
+  @Override
+  protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+    return application.sources(Application.class);
+  }
+
+  public static void main(String[] args) throws Exception {
+    SpringApplication.run(Application.class, args);
+  }
+
+}
