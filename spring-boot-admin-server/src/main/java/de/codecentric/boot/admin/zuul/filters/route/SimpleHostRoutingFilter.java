3dbea6bf14cea276122d8ef24d53409defae3263
Johannes Edmeier <johannes.edmeier@codecentric.de>
2017-02-10 14:04:48 +0100

    Fix content-type parsing in SimleHostRoutingFilter
    Posting via zuul doesnt work in case the Content-Type has a charset
    specified.
    Applied fix from spring-cloud/spring-cloud-netflix#1663
    
    fixes #403

--- spring-boot-admin-server/src/main/java/de/codecentric/boot/admin/zuul/filters/route/SimpleHostRoutingFilter.java
+++ spring-boot-admin-server/src/main/java/de/codecentric/boot/admin/zuul/filters/route/SimpleHostRoutingFilter.java
@@ -282,7 +282,7 @@ public class SimpleHostRoutingFilter extends ZuulFilter {
 		int contentLength = request.getContentLength();
 		InputStreamEntity entity = new InputStreamEntity(requestEntity, contentLength,
 				request.getContentType() != null
-						? ContentType.create(request.getContentType()) : null);
+						? ContentType.parse(request.getContentType()) : null);
 		switch (verb.toUpperCase()) {
 		case "POST":
 			HttpPost httpPost = new HttpPost(uri + this.helper.getQueryString(params));
