4fa0b7021289317fda8ebc8ef2878533a856bfcc
Johannes Edmeier <johannes.edmeier@codecentric.de>
2017-03-28 09:34:01 +0200

    Add Workaround for spring-cloud-netflix#1815
    Adds a workaround for
    https://github.com/spring-cloud/spring-cloud-netflix/issues/1815
    by including spring-cloud-context to the server's dependencies

---
+++ spring-boot-admin-samples/spring-boot-admin-sample/src/test/java/de/codecentric/boot/admin/SpringBootAdminApplicationTest.java
@@ -0,0 +1,14 @@
+package de.codecentric.boot.admin;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = { SpringBootAdminApplication.class })
+public class SpringBootAdminApplicationTest {
+	@Test
+	public void contextLoads() {
+	}
+}
