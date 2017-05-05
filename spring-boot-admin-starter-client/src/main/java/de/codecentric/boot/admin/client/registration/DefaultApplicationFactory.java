6540a50eced9373b9fb112eacfe287eae4ebd049
coestre <christian.oestreich@optum.com>
2017-05-05 10:18:31 -0500

    Codacy code cleanup

--- spring-boot-admin-starter-client/src/main/java/de/codecentric/boot/admin/client/registration/DefaultApplicationFactory.java
+++ spring-boot-admin-starter-client/src/main/java/de/codecentric/boot/admin/client/registration/DefaultApplicationFactory.java
@@ -4,7 +4,6 @@ import static org.springframework.util.StringUtils.trimLeadingCharacter;
 
 import java.net.InetAddress;
 import java.net.UnknownHostException;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
