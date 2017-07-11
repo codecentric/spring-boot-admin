6ff4a222e18e6dacf7aae9a5ca6fe5fbc883bad4
Daniel van 't Oever <dvtoever@quintor.nl>
2017-07-11 13:28:06 +0200

    - removed unused import

--- spring-boot-admin-server-ui-javamelody/src/main/java/net/bull/javamelody/JavaMelodyService.java
+++ spring-boot-admin-server-ui-javamelody/src/main/java/net/bull/javamelody/JavaMelodyService.java
@@ -6,7 +6,6 @@ import de.codecentric.boot.admin.model.Application;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.event.EventListener;
-import org.springframework.stereotype.Service;
 
 import java.io.IOException;
 
