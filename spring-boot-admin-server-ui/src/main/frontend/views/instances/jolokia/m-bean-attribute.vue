25f4003f807da4e4561953c9172fcf5b573d74e2
Johannes Edmeier <johannes.edmeier@codecentric.de>
2018-02-19 08:03:17 +0100

    Allow esc/enter to cancel/save when edition MBean attributes.

--- spring-boot-admin-server-ui/src/main/frontend/views/instances/jolokia/m-bean-attribute.vue
+++ spring-boot-admin-server-ui/src/main/frontend/views/instances/jolokia/m-bean-attribute.vue
@@ -29,14 +29,17 @@
             </button>
           </div>
           <div class="control is-expanded has-icons-right">
-            <input v-if="!hasComplexValue" class="input" type="text" :readonly="!editing"
-                   :value="editing? input : value" @input="input = $event.target.value"
-                   @dblclick="edit">
-            <textarea v-else class="input m-bean-attribute--text" :readonly="!editing"
-                      v-text="jsonValue"/>
-            <span class="icon is-right has-text-warning" v-if="error">
-              <font-awesome-icon icon="exclamation-triangle"/>
-            </span>
+            <template v-if="!editing">
+              <input v-if="!hasComplexValue" class="input" type="text" readonly :value="value" @dblclick="edit">
+              <textarea v-else class="input m-bean-attribute--text" readonly v-text="jsonValue"/>
+              <span class="icon is-right has-text-warning" v-if="error">
+                <font-awesome-icon icon="exclamation-triangle"/>
+              </span>
+            </template>
+            <template v-else>
+              <input v-if="!hasComplexValue" class="input" type="text" ref="input"
+                     v-model="input" @keyup.esc="cancel" @keyup.enter="save">
+            </template>
           </div>
         </div>
         <div class="help" v-text="descriptor.desc"/>
@@ -86,10 +89,12 @@
       },
     },
     methods: {
-      edit() {
-        if (this.descriptor.rw) {
+      async edit() {
+        if (this.descriptor.rw && !this.hasComplexValue) {
           this.input = this.value;
           this.editing = true;
+          await this.$nextTick();
+          this.$refs.input.focus();
         }
       },
       cancel() {
