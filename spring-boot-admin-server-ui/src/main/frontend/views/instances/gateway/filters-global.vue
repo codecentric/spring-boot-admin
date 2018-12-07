00a8836abc8f33a0aec87a0c45f0b0c455be8872
Stevan Kosijer <s.kosijer@levi9.com>
2018-12-07 17:33:08 +0100

    Transform returned global filter data (#993)

--- spring-boot-admin-server-ui/src/main/frontend/views/instances/gateway/filters-global.vue
+++ spring-boot-admin-server-ui/src/main/frontend/views/instances/gateway/filters-global.vue
@@ -1,9 +1,25 @@
+<!--
+  - Copyright 2014-2018 the original author or authors.
+  -
+  - Licensed under the Apache License, Version 2.0 (the "License");
+  - you may not use this file except in compliance with the License.
+  - You may obtain a copy of the License at
+  -
+  -     http://www.apache.org/licenses/LICENSE-2.0
+  -
+  - Unless required by applicable law or agreed to in writing, software
+  - distributed under the License is distributed on an "AS IS" BASIS,
+  - WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  - See the License for the specific language governing permissions and
+  - limitations under the License.
+  -->
+
 <template>
   <section class="section" :class="{ 'is-loading' : !hasLoaded }">
     <div v-if="error" class="message is-danger">
       <div class="message-body">
         <strong>
-          <font-awesome-icon class="has-text-danger" icon="exclamation-triangle" />
+          <FontAwesomeIcon class="has-text-danger" icon="exclamation-triangle" />
           Fetching global filters failed.
         </strong>
         <p v-text="error.message" />
@@ -12,7 +28,7 @@
 
     <div class="field has-addons" v-if="globalFilters">
       <p class="control is-expanded">
-        <input class="input" type="search" placeholder="name filter" v-model="globalFilterFilter">
+        <input class="input" type="search" placeholder="name filter" v-model="globalFilterSearch">
       </p>
     </div>
 
@@ -35,7 +51,7 @@
       hasLoaded: false,
       error: null,
       globalFilters: null,
-      globalFilterFilter: null
+      globalFilterSearch: null
     }),
     computed: {
       hasGlobalFiltersData: function () {
@@ -50,7 +66,9 @@
         this.error = null;
         try {
           const res = await this.instance.fetchGlobalFiltersData();
-          this.globalFilters = res.data;
+          this.globalFilters = Object.keys(res.data).map(function(key) {
+            return {name: key, order: res.data[key]};
+          });
         } catch (error) {
           console.warn('Fetching global filters failed:', error);
           this.error = error;
