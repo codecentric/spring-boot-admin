2b8a0132c0e2585ef8821cbed1b9d2a3e09dad97
Johannes Edmeier <johannes.edmeier@codecentric.de>
2019-01-05 21:11:41 +0100

    Upgrade npm dependencies

--- spring-boot-admin-server-ui/src/main/frontend/views/journal/index.vue
+++ spring-boot-admin-server-ui/src/main/frontend/views/journal/index.vue
@@ -1,5 +1,5 @@
 <!--
-  - Copyright 2014-2018 the original author or authors.
+  - Copyright 2014-2019 the original author or authors.
   -
   - Licensed under the Apache License, Version 2.0 (the "License");
   - you may not use this file except in compliance with the License.
@@ -29,7 +29,7 @@
           <p v-text="error.message" />
         </div>
       </div>
-      <table class="table is-fullwidth">
+      <table class="table is-fullwidth is-hoverable">
         <thead>
           <tr>
             <th>Application</th>
