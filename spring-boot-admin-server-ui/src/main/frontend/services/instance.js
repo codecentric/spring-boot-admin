8b92e026ab7aede10b96c3ad7b954c9806905652
Johannes Edmeier <johannes.edmeier@codecentric.de>
2019-01-31 22:25:27 +0100

    Don't validate the http status for health
    As 5xx response statuses are valid for the health endpoint, don't
    validate the response status on the client side for it.
    
    fixes #1078

--- spring-boot-admin-server-ui/src/main/frontend/services/instance.js
+++ spring-boot-admin-server-ui/src/main/frontend/services/instance.js
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014-2018 the original author or authors.
+ * Copyright 2014-2019 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -80,7 +80,8 @@ class Instance {
 
   async fetchHealth() {
     return await this.axios.get(uri`actuator/health`, {
-      headers: {'Accept': actuatorMimeTypes}
+      headers: {'Accept': actuatorMimeTypes},
+      validateStatus: null
     });
   }
 
