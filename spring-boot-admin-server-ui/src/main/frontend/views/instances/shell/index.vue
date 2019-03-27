c95af61271a10c4bc986b620836bdbffd2da486f
Johannes Edmeier <johannes.edmeier@codecentric.de>
2019-03-27 23:05:05 +0100

    Prevent instace view to be expanded beyound viewport
    fixes #1132

--- spring-boot-admin-server-ui/src/main/frontend/views/instances/shell/index.vue
+++ spring-boot-admin-server-ui/src/main/frontend/views/instances/shell/index.vue
@@ -1,5 +1,5 @@
 <!--
-  - Copyright 2014-2018 the original author or authors.
+  - Copyright 2014-2019 the original author or authors.
   -
   - Licensed under the Apache License, Version 2.0 (the "License");
   - you may not use this file except in compliance with the License.
@@ -96,6 +96,7 @@
       flex-grow: 1;
       flex-shrink: 1;
       z-index: 10;
+      max-width: calc(100vw - 175px); /*sidebar-width*/
     }
   }
 </style>
