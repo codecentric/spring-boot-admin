2f321ad9649f8a3ff7a2070a2ed4d010dca6eb8e
Johannes Edmeier <johannes.edmeier@codecentric.de>
2019-05-05 21:50:59 +0200

    Fix InstanceExchangeFilterFunction sample: Don't log multiple times

--- spring-boot-admin-samples/spring-boot-admin-sample-servlet/src/main/java/de/codecentric/boot/admin/SpringBootAdminServletApplication.java
+++ spring-boot-admin-samples/spring-boot-admin-sample-servlet/src/main/java/de/codecentric/boot/admin/SpringBootAdminServletApplication.java
@@ -45,12 +45,11 @@ public class SpringBootAdminServletApplication {
     // tag::customization-instance-exchange-filter-function[]
     @Bean
     public InstanceExchangeFilterFunction auditLog() {
-        return (instance, request, next) -> {
+        return (instance, request, next) -> next.exchange(request).doOnSubscribe(s -> {
             if (HttpMethod.DELETE.equals(request.method()) || HttpMethod.POST.equals(request.method())) {
                 log.info("{} for {} on {}", request.method(), instance.getId(), request.url());
             }
-            return next.exchange(request);
-        };
+        });
     }
     // end::customization-instance-exchange-filter-function[]
 
