f7ea582d3ac88f1a18cb329d8342df108304a10a
Stephan Köninger <github@stekoe.de>
2021-07-09 12:11:25 +0200

    #1729: Implement first draft of Quartz Actuator view

--- spring-boot-admin-server-ui/src/main/frontend/services/instance.js
+++ spring-boot-admin-server-ui/src/main/frontend/services/instance.js
@@ -278,6 +278,31 @@ class Instance {
     });
   }
 
+  async fetchQuartzJobs() {
+    return this.axios.get(uri`actuator/quartz/jobs`, {
+      headers: {'Accept': 'application/json'}
+    });
+  }
+
+  async fetchQuartzJob(group, name) {
+    return this.axios.get(uri`actuator/quartz/jobs/${group}/${name}`, {
+      headers: {'Accept': 'application/json'}
+    });
+  }
+
+  async fetchQuartzTriggers() {
+    return this.axios.get(uri`actuator/quartz/triggers`, {
+      headers: {'Accept': 'application/json'}
+    });
+  }
+
+  async fetchQuartzTrigger(group, name) {
+    return this.axios.get(uri`actuator/quartz/triggers/${group}/${name}`, {
+      headers: {'Accept': 'application/json'}
+    });
+  }
+
+
   static getEventStream() {
     return concat(
       from(waitForPolyfill()).pipe(ignoreElements()),
