6e432b9a5fc7bfc1eb64f7d497de4852dae29eca
Stephan Köninger <stephan.koeninger.ext@devk.de>
2021-07-31 17:35:49 +0200

    add more stories

---
+++ spring-boot-admin-server-ui/src/main/frontend/components/sba-tags.stories.js
@@ -0,0 +1,20 @@
+import SbaTags from './sba-tags';
+
+export default {
+  component: SbaTags,
+  title: 'SBA Components/Tags',
+};
+
+const Template = (args, {argTypes}) => ({
+  components: {SbaTags},
+  props: Object.keys(argTypes),
+  template: '<sba-tags v-bind="$props" />'
+});
+
+export const Tags = Template.bind({});
+Tags.args = {
+  tags: {
+    "This is a key": "This a value",
+    simpleKey: "value",
+  }
+};
