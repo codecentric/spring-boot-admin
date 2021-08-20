2571e1849feb7ffbf7d34a9b303595338e348f62
Erik Petzold <erik.petzold@codecentric.de>
2021-08-20 15:16:22 +0200

    #1729: add more details to quartz triggers (#1797)

---
+++ spring-boot-admin-server-ui/src/main/frontend/views/instances/quartz/trigger-row.spec.js
@@ -0,0 +1,38 @@
+import {render} from '@/test-utils';
+import TriggerRow from './trigger-row';
+import {screen} from '@testing-library/vue';
+import userEvent from '@testing-library/user-event';
+
+
+describe('trigger-row.vue', () => {
+
+  beforeEach(async () =>{
+    await render(TriggerRow, {
+      props: {
+        triggerDetail: {
+          'group': 'group2',
+          'name': 'triggerSampleJob2',
+          'state': 'NORMAL',
+          'type': 'simple',
+          'startTime': 1629451789546,
+          'previousFireTime': 1629452183546,
+          'nextFireTime': 1629452185546,
+          'priority': 0,
+          'simple': {'interval': 2000, 'repeatCount': -1, 'timesTriggered': 198}
+        }
+      }
+    })
+  })
+
+  it('should render with closed details initially', async () => {
+    const intervalElement = await screen.queryByText('interval')
+    expect(intervalElement).toBeNull()
+  })
+
+  it('should render details when clicking on arrow', async () => {
+    const toggleArrow = await screen.findByRole('button')
+    userEvent.click(toggleArrow)
+    const intervalElement = await screen.findByText('interval')
+    expect(intervalElement).toBeVisible()
+  })
+})
