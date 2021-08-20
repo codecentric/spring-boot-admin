8314a9608895baf0694dfb2d4801da954f55fe73
fiveOO <48786640+fiveOO@users.noreply.github.com>
2021-08-20 21:11:33 +0200

    Added tests for CacheHandler creation

--- spring-boot-admin-server/src/test/java/de/codecentric/boot/admin/server/web/client/cookies/JdkPerInstanceCookieStoreTest.java
+++ spring-boot-admin-server/src/test/java/de/codecentric/boot/admin/server/web/client/cookies/JdkPerInstanceCookieStoreTest.java
@@ -25,7 +25,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 
-import de.codecentric.boot.admin.server.domain.entities.Instance;
 import de.codecentric.boot.admin.server.domain.values.InstanceId;
 
 import static java.util.Collections.singletonList;
@@ -34,11 +33,12 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 class JdkPerInstanceCookieStoreTest {
 
-	private static final Instance INSTANCE = Instance.create(InstanceId.of("i"));
+	private static final InstanceId INSTANCE_ID = InstanceId.of("i");
 
 	private CookieHandler cookieHandler;
 
@@ -49,7 +49,7 @@ class JdkPerInstanceCookieStoreTest {
 		store = spy(new JdkPerInstanceCookieStore());
 		cookieHandler = mock(CookieHandler.class);
 
-		when(store.createCookieHandler(INSTANCE.getId())).thenReturn(cookieHandler);
+		when(store.createCookieHandler(INSTANCE_ID)).thenReturn(cookieHandler);
 	}
 
 	@Test
@@ -61,10 +61,37 @@ class JdkPerInstanceCookieStoreTest {
 		final URI uri = URI.create("http://localhost/test");
 		when(cookieHandler.get(eq(uri), any())).thenReturn(storeMap);
 
-		final MultiValueMap<String, String> cookieMap = store.get(INSTANCE.getId(), uri, new LinkedMultiValueMap<>());
+		final MultiValueMap<String, String> cookieMap = store.get(INSTANCE_ID, uri, new LinkedMultiValueMap<>());
 
 		assertThat(cookieMap).containsEntry("name", singletonList("value"));
 		assertThat(cookieMap).containsEntry("name2", singletonList("tricky=value"));
+	}
+
+	@Test
+	void same_handler_should_be_used_for_same_instance_id_on_different_uri() throws IOException {
+		MultiValueMap<String, String> storeMap = new LinkedMultiValueMap<>();
+		final URI uri = URI.create("http://localhost/test");
+		final URI uri2 = URI.create("http://localhost/test2");
+		store.get(INSTANCE_ID, uri, storeMap);
+		store.get(INSTANCE_ID, uri2, storeMap);
+
+		verify(cookieHandler).get(uri, storeMap);
+		verify(cookieHandler).get(uri2, storeMap);
+	}
+
+	@Test
+	void different_handler_should_be_used_for_different_instance_id() throws IOException {
+		InstanceId INSTANCE_ID2 = InstanceId.of("j");
+		CookieHandler cookieHandler2 = mock(CookieHandler.class);
+		when(store.createCookieHandler(INSTANCE_ID2)).thenReturn(cookieHandler2);
+
+		MultiValueMap<String, String> storeMap = new LinkedMultiValueMap<>();
+		final URI uri = URI.create("http://localhost/test");
+		store.get(INSTANCE_ID, uri, storeMap);
+		store.get(INSTANCE_ID2, uri, storeMap);
+
+		verify(cookieHandler).get(uri, storeMap);
+		verify(cookieHandler2).get(uri, storeMap);
 	}
 
 }
