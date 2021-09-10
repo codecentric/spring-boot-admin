90fa80c77dffd91ee4fb4f044f5260ee4dbb77e9
erik.petzold <erik.petzold@codecentric.de>
2021-09-10 09:23:30 +0200

    formatted with spring-javaformat:apply

--- spring-boot-admin-server/src/main/java/de/codecentric/boot/admin/server/web/client/InstanceWebClient.java
+++ spring-boot-admin-server/src/main/java/de/codecentric/boot/admin/server/web/client/InstanceWebClient.java
@@ -62,11 +62,9 @@ public class InstanceWebClient {
 	}
 
 	private static ExchangeFilterFunction toExchangeFilterFunction(InstanceExchangeFilterFunction filter) {
-		return (request, next) -> request.attribute(ATTRIBUTE_INSTANCE)
-			.filter(Instance.class::isInstance)
-			.map(Instance.class::cast)
-			.map((instance) -> filter.filter(instance, request, next))
-			.orElseGet(() -> next.exchange(request));
+		return (request, next) -> request.attribute(ATTRIBUTE_INSTANCE).filter(Instance.class::isInstance)
+				.map(Instance.class::cast).map((instance) -> filter.filter(instance, request, next))
+				.orElseGet(() -> next.exchange(request));
 	}
 
 	public static class Builder {
