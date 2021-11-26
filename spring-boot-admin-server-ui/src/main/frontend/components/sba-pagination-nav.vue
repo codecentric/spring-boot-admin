0670a25294b335e5cd09d5c96a98b33a89bb8e04
Stephan Köninger <github@stekoe.de>
2021-11-26 10:07:35 +0100

    Enhancement as suggested in #1859: Add pagination to journal (#1862)
    closes #1859
    
    * Enhancement as suggested in #1859: Add pagination to journal
    
    * Use v-model
    
    * Bump to 2.5.5-SNAPSHOT
    
    Co-authored-by: Ulrich Schulte <ulrich.schulte@codecentric.de>

---
+++ spring-boot-admin-server-ui/src/main/frontend/components/sba-pagination-nav.vue
@@ -0,0 +1,57 @@
+<!--
+  - Copyright 2014-2019 the original author or authors.
+  -
+  - Licensed under the Apache License, Version 2.0 (the "License");
+  - you may not use this file except in compliance with the License.
+  - You may obtain a copy of the License at
+  -
+  -     http://www.apache.org/licenses/LICENSE-2.0
+  -
+  - Unless required by applicable law or agreed to in writing, software
+  - distributed under the License is distributed on an "AS IS" BASIS,
+  - WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  - See the License for the specific language governing permissions and
+  - limitations under the License.
+  -->
+
+<template>
+  <div class="field has-addons has-addons-centered">
+    <p class="control">
+      <button class="button" @click="$emit('change', current-1)" :disabled="current <= 1">
+        <span class="icon is-small">
+          <font-awesome-icon :icon="['fas','angle-double-left']" />
+        </span>
+      </button>
+    </p>
+    <p class="control" v-for="page in pageCount" :key="'page' + page">
+      <button class="button" :class="{'is-active': page === current}" @click="$emit('change', page)">
+        <span>{{ page }}</span>
+      </button>
+    </p>
+    <p class="control">
+      <button class="button" @click="$emit('change', current+1)" :disabled="current >= pageCount">
+        <span class="icon is-small">
+          <font-awesome-icon :icon="['fas','angle-double-right']" />
+        </span>
+      </button>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SbaPaginationNav',
+  model: {
+    prop: 'current',
+    event: 'change'
+  },
+  props: {
+    current: {type: Number, default: 1},
+    pageCount: {type: Number, required: true}
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
