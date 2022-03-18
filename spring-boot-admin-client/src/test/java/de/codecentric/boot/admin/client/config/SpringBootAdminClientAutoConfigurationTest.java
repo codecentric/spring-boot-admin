81eb97bca46797b72a0b02bb20a456e8693131f7
Fedor Bobin <fuudtorrentsru@gmail.com>
2022-03-18 11:19:39 +0300

    Allow overriding registration client in blocking or reactive env (#2004)

--- spring-boot-admin-client/src/test/java/de/codecentric/boot/admin/client/config/SpringBootAdminClientAutoConfigurationTest.java
+++ spring-boot-admin-client/src/test/java/de/codecentric/boot/admin/client/config/SpringBootAdminClientAutoConfigurationTest.java
@@ -23,11 +23,17 @@ import org.springframework.boot.autoconfigure.AutoConfigurations;
 import org.springframework.boot.autoconfigure.web.client.RestTemplateAutoConfiguration;
 import org.springframework.boot.autoconfigure.web.reactive.function.client.WebClientAutoConfiguration;
 import org.springframework.boot.autoconfigure.web.servlet.DispatcherServletAutoConfiguration;
+import org.springframework.boot.context.annotation.UserConfigurations;
 import org.springframework.boot.test.context.runner.ApplicationContextRunner;
 import org.springframework.boot.test.context.runner.ReactiveWebApplicationContextRunner;
 import org.springframework.boot.test.context.runner.WebApplicationContextRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
 
 import de.codecentric.boot.admin.client.registration.ApplicationRegistrator;
+import de.codecentric.boot.admin.client.registration.BlockingRegistrationClient;
+import de.codecentric.boot.admin.client.registration.RegistrationClient;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -74,6 +80,46 @@ public class SpringBootAdminClientAutoConfigurationTest {
 								WebClientAutoConfiguration.class, SpringBootAdminClientAutoConfiguration.class));
 		reactiveContextRunner.withPropertyValues("spring.boot.admin.client.url:http://localhost:8081")
 				.run((context) -> assertThat(context).hasSingleBean(ApplicationRegistrator.class));
+	}
+
+	@Test
+	public void customBlockingClientInReactiveEnvironment() {
+		ReactiveWebApplicationContextRunner reactiveContextRunner = new ReactiveWebApplicationContextRunner()
+				.withConfiguration(UserConfigurations.of(CustomBlockingConfiguration.class)).withConfiguration(
+						AutoConfigurations.of(EndpointAutoConfiguration.class, WebEndpointAutoConfiguration.class,
+								WebClientAutoConfiguration.class, SpringBootAdminClientAutoConfiguration.class));
+		reactiveContextRunner.withPropertyValues("spring.boot.admin.client.url:http://localhost:8081")
+				.run((context) -> {
+					assertThat(context).hasSingleBean(ApplicationRegistrator.class);
+					assertThat(context).getBean("registrationClient")
+							.isEqualTo(context.getBean(CustomBlockingConfiguration.class).registrationClient);
+				});
+	}
+
+	@Test
+	public void customBlockingClientInBlockingEnvironment() {
+		ReactiveWebApplicationContextRunner reactiveContextRunner = new ReactiveWebApplicationContextRunner()
+				.withConfiguration(UserConfigurations.of(CustomBlockingConfiguration.class)).withConfiguration(
+						AutoConfigurations.of(EndpointAutoConfiguration.class, WebEndpointAutoConfiguration.class,
+								RestTemplateAutoConfiguration.class, SpringBootAdminClientAutoConfiguration.class));
+		reactiveContextRunner.withPropertyValues("spring.boot.admin.client.url:http://localhost:8081")
+				.run((context) -> {
+					assertThat(context).hasSingleBean(ApplicationRegistrator.class);
+					assertThat(context).getBean("registrationClient")
+							.isEqualTo(context.getBean(CustomBlockingConfiguration.class).registrationClient);
+				});
+	}
+
+	@Configuration
+	public static class CustomBlockingConfiguration {
+
+		RegistrationClient registrationClient = new BlockingRegistrationClient(new RestTemplate());
+
+		@Bean
+		public RegistrationClient registrationClient() {
+			return registrationClient;
+		}
+
 	}
 
 }
