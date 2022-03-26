918e0303c9fa766b564d4113ee1fa0772f768807
Omar Espino <37581410+oespino@users.noreply.github.com>
2022-03-26 10:11:35 +0100

    Emit event when scopes changes (#2006)

--- spring-boot-admin-server-ui/src/main/frontend/views/instances/loggers/loggers.vue
+++ spring-boot-admin-server-ui/src/main/frontend/views/instances/loggers/loggers.vue
@@ -23,6 +23,7 @@
         <div class="control">
           <sba-toggle-scope-button v-if="instanceCount > 1"
                                    v-model="scope"
+                                   @changeScope="$emit('changeScope', $event)"
                                    :instance-count="instanceCount"
           />
         </div>
