746790ca93c9a27d1ba4b0971ecc447aa8caa2dc
Stephan Köninger <stephan.koeninger@codecentric.de>
2022-04-22 20:04:40 +0200

    no whitespaces.

--- spring-boot-admin-server-ui/src/main/frontend/views/instances/jolokia/m-bean-operation-invocation.vue
+++ spring-boot-admin-server-ui/src/main/frontend/views/instances/jolokia/m-bean-operation-invocation.vue
@@ -105,134 +105,134 @@
 </template>
 
 <script>
-export default {
-  props: {
-    name: {
-      type: String,
-      required: true
-    },
-    descriptor: {
-      type: Object,
-      required: true
-    },
-    value: {
-      type: null,
-      default: null
-    },
-    onClose: {
-      type: Function,
-      required: true
-    },
-    onExecute: {
-      type: Function,
-      required: true
-    }
-  },
-  data: () => ({
-    state: null,
-    error: null,
-    args: null,
-    result: null
-  }),
-  computed: {
-    prettyPrintedResult() {
-      if (this.result && typeof this.result === 'string') {
-        try {
-          const o = JSON.parse(this.result);
-          return JSON.stringify(o, undefined, 4);
-        } catch (e) {
-          return this.result;
-        }
-      } else if (typeof result === 'object') {
-        return JSON.stringify(this.result, undefined, 4);
-      }
-      return this.result;
-    }
-  },
-  methods: {
-    abort() {
-      this.onClose();
-    },
-    invoke(args) {
-      this.state = (args || this.descriptor.args.length === 0) ? 'prepared' : 'input-args';
-      this.args = args || new Array(this.descriptor.args.length);
-      this.error = null;
-      this.result = null;
-
-      if (this.state === 'prepared') {
-        this.execute()
+  export default {
+    props: {
+      name: {
+        type: String,
+        required: true
+      },
+      descriptor: {
+        type: Object,
+        required: true
+      },
+      value: {
+        type: null,
+        default: null
+      },
+      onClose: {
+        type: Function,
+        required: true
+      },
+      onExecute: {
+        type: Function,
+        required: true
       }
     },
-    async execute() {
-      this.state = 'executing';
-      try {
-        const result = await this.onExecute(this.args);
-
-        if (!this.resultContainsErrorStatus(result)) {
-          this.result = this.parseValue(result.data);
-          this.state = 'completed';
-        } else {
-          const parsedResult = this.parseValue(result.data);
-          let failedRequest = result.data;
-
-          // Show first failed request
-          if (Array.isArray(result.data)) {
-            failedRequest = parsedResult[0];
+    data: () => ({
+      state: null,
+      error: null,
+      args: null,
+      result: null
+    }),
+    computed: {
+      prettyPrintedResult() {
+        if (this.result && typeof this.result === 'string') {
+          try {
+            const o = JSON.parse(this.result);
+            return JSON.stringify(o, undefined, 4);
+          } catch (e) {
+            return this.result;
           }
+        } else if (typeof result === 'object') {
+          return JSON.stringify(this.result, undefined, 4);
+        }
+        return this.result;
+      }
+    },
+    methods: {
+      abort() {
+        this.onClose();
+      },
+      invoke(args) {
+        this.state = (args || this.descriptor.args.length === 0) ? 'prepared' : 'input-args';
+        this.args = args || new Array(this.descriptor.args.length);
+        this.error = null;
+        this.result = null;
 
-          const error = new Error(`Execution failed: ${failedRequest.error}`);
-          error.stacktrace = failedRequest.stacktrace;
+        if (this.state === 'prepared') {
+          this.execute()
+        }
+      },
+      async execute() {
+        this.state = 'executing';
+        try {
+          const result = await this.onExecute(this.args);
+
+          if (!this.resultContainsErrorStatus(result)) {
+            this.result = this.parseValue(result.data);
+            this.state = 'completed';
+          } else {
+            const parsedResult = this.parseValue(result.data);
+            let failedRequest = result.data;
+
+            // Show first failed request
+            if (Array.isArray(result.data)) {
+              failedRequest = parsedResult[0];
+            }
+
+            const error = new Error(`Execution failed: ${failedRequest.error}`);
+            error.stacktrace = failedRequest.stacktrace;
+            this.state = 'failed';
+            this.error = error;
+            console.warn('Invocation failed', error);
+          }
+        } catch (error) {
           this.state = 'failed';
           this.error = error;
           console.warn('Invocation failed', error);
         }
-      } catch (error) {
-        this.state = 'failed';
-        this.error = error;
-        console.warn('Invocation failed', error);
-      }
-    },
-    keyHandler(event) {
-      if (event.keyCode === 27) {
-        this.abort()
-      }
-    },
-    resultContainsErrorStatus(result) {
-      if (result.status >= 400) {
-        return true;
-      }
+      },
+      keyHandler(event) {
+        if (event.keyCode === 27) {
+          this.abort()
+        }
+      },
+      resultContainsErrorStatus(result) {
+        if (result.status >= 400) {
+          return true;
+        }
 
-      const parsedResponse = this.parseValue(result.data);
-      if (Array.isArray(parsedResponse)) {
-        return parsedResponse.some(r => r.status >= 400);
-      } else {
-        return result.data.status >= 400;
+        const parsedResponse = this.parseValue(result.data);
+        if (Array.isArray(parsedResponse)) {
+          return parsedResponse.some(r => r.status >= 400);
+        } else {
+          return result.data.status >= 400;
+        }
+      },
+      parseValue(data) {
+        if (Array.isArray(data)) {
+          return data.map(elem => {
+            const parsedBody = JSON.parse(elem['body']);
+            return {
+              instanceId: elem['instanceId'],
+              ...parsedBody
+            };
+          });
+        } else {
+          return data.value;
+        }
       }
     },
-    parseValue(data) {
-      if (Array.isArray(data)) {
-        return data.map(elem => {
-          const parsedBody = JSON.parse(elem['body']);
-          return {
-            instanceId: elem['instanceId'],
-            ...parsedBody
-          };
-        });
-      } else {
-        return data.value;
-      }
-    }
-  },
-  created() {
-    this.invoke();
-  },
-  mounted() {
-    document.addEventListener('keyup', this.keyHandler)
-  },
-  beforeDestroy() {
-    document.removeEventListener('keyup', this.keyHandler)
-  },
-}
+    created() {
+      this.invoke();
+    },
+    mounted() {
+      document.addEventListener('keyup', this.keyHandler)
+    },
+    beforeDestroy() {
+      document.removeEventListener('keyup', this.keyHandler)
+    },
+  }
 </script>
 
 <style scoped>
