c7c20f025b27efd03217eabc16d67dbcea5d9edd
Stephan Köninger <stephan.koeninger@codecentric.de>
2022-07-08 11:23:28 +0200

    chore: polishing PR

--- spring-boot-admin-samples/spring-boot-admin-sample-reactive/src/main/java/de/codecentric/boot/admin/SpringBootAdminReactiveApplication.java
+++ spring-boot-admin-samples/spring-boot-admin-sample-reactive/src/main/java/de/codecentric/boot/admin/SpringBootAdminReactiveApplication.java
@@ -26,6 +26,8 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.authentication.RedirectServerAuthenticationSuccessHandler;
+import org.springframework.security.web.server.authentication.ServerAuthenticationSuccessHandler;
 import org.springframework.security.web.server.authentication.logout.RedirectServerLogoutSuccessHandler;
 import org.springframework.security.web.server.authentication.logout.ServerLogoutSuccessHandler;
 
@@ -64,16 +66,24 @@ public class SpringBootAdminReactiveApplication {
 						.pathMatchers(this.adminServer.path("/assets/**")).permitAll()
 						.pathMatchers("/actuator/health/**").permitAll().pathMatchers(this.adminServer.path("/login"))
 						.permitAll().anyExchange().authenticated())
-				.formLogin((formLogin) -> formLogin.loginPage(this.adminServer.path("/login")))
-				.logout((logout) -> logout
-						.logoutUrl(this.adminServer.path("/logout"))
+				.formLogin((formLogin) -> formLogin.loginPage(this.adminServer.path("/login"))
+						.authenticationSuccessHandler(loginSuccessHandler(this.adminServer.path("/"))))
+				.logout((logout) -> logout.logoutUrl(this.adminServer.path("/logout"))
 						.logoutSuccessHandler(logoutSuccessHandler(this.adminServer.path("/login?logout"))))
 				.httpBasic(Customizer.withDefaults()).csrf(ServerHttpSecurity.CsrfSpec::disable).build();
 	}
 
-	public ServerLogoutSuccessHandler logoutSuccessHandler(String uri) {
+	// The following two methods are only required when setting a custom base-path (see
+	// 'basepath' profile in application.yml)
+	private ServerLogoutSuccessHandler logoutSuccessHandler(String uri) {
 		RedirectServerLogoutSuccessHandler successHandler = new RedirectServerLogoutSuccessHandler();
 		successHandler.setLogoutSuccessUrl(URI.create(uri));
+		return successHandler;
+	}
+
+	private ServerAuthenticationSuccessHandler loginSuccessHandler(String uri) {
+		RedirectServerAuthenticationSuccessHandler successHandler = new RedirectServerAuthenticationSuccessHandler();
+		successHandler.setLocation(URI.create(uri));
 		return successHandler;
 	}
 
