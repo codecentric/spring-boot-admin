f2baeb0f716a35624c0c9b6094762e1bf264f97d
qxo <49526356@qq.com>
2022-10-26 00:14:44 +0800

    fix: 406 sba-settings.js

--- spring-boot-admin-server-ui/src/main/java/de/codecentric/boot/admin/server/ui/web/UiController.java
+++ spring-boot-admin-server-ui/src/main/java/de/codecentric/boot/admin/server/ui/web/UiController.java
@@ -101,7 +101,7 @@ public class UiController {
 		return "index";
 	}
 
-	@GetMapping(path = "/sba-settings.js", produces = "application/javascript")
+	@GetMapping(path = "/sba-settings.js")
 	public String sbaSettings() {
 		return "sba-settings.js";
 	}
