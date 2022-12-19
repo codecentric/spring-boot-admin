98ea756aee1d1aca3437bceca472774763d5b1bb
Stephan Köninger <stephan.koeninger@codecentric.de>
2022-12-19 12:24:11 +0100

    chore: add tests for useApplicationStore

--- spring-boot-admin-server-ui/src/main/frontend/composables/useApplicationStore.js
+++ spring-boot-admin-server-ui/src/main/frontend/composables/useApplicationStore.js
@@ -9,32 +9,27 @@ const error = ref(null);
 
 export function createApplicationStore() {
   applicationStore = new ApplicationStore();
-  console.log('createApplicationStore');
   return applicationStore;
 }
 
 export function useApplicationStore() {
   applicationStore.addEventListener('connected', () => {
-    console.log('connected');
     applicationsInitialized.value = true;
     error.value = null;
   });
 
   applicationStore.addEventListener('changed', (newApplications) => {
-    console.log('changed');
     applicationsInitialized.value = true;
     applications.value = newApplications;
     error.value = null;
   });
 
   applicationStore.addEventListener('error', (errorResponse) => {
-    console.log('error');
     applicationsInitialized.value = true;
     error.value = errorResponse;
   });
 
   applicationStore.addEventListener('removed', () => {
-    console.log('removed');
     applicationsInitialized.value = false;
   });
 
---
+++ spring-boot-admin-server-ui/src/main/frontend/store.spec.js
@@ -0,0 +1,109 @@
+import { waitFor } from '@testing-library/vue';
+import { rest } from 'msw';
+import { ReplaySubject } from 'rxjs';
+
+import { registerWithOneInstance } from '@/mocks/fixtures/eventStream/registerWithOneInstance';
+import { registerWithTwoInstances } from '@/mocks/fixtures/eventStream/registerWithTwoInstances';
+import { server } from '@/mocks/server';
+import Application from '@/services/application';
+import ApplicationStore from '@/store';
+
+server.use(
+  rest.get('/applications', (req, res, ctx) => {
+    return res(ctx.status(200), ctx.json([]));
+  })
+);
+
+describe('store.js', () => {
+  let applicationStore;
+
+  let mockSubject;
+  jest.spyOn(Application, 'getStream').mockImplementation(() => mockSubject);
+
+  const changedListener = jest.fn();
+  const addedListener = jest.fn();
+  const updateListener = jest.fn();
+  const removedListener = jest.fn();
+
+  beforeEach(() => {
+    mockSubject = new ReplaySubject();
+    applicationStore = new ApplicationStore();
+    applicationStore.start();
+    applicationStore.addEventListener('changed', changedListener);
+    applicationStore.addEventListener('added', addedListener);
+    applicationStore.addEventListener('updated', updateListener);
+    applicationStore.addEventListener('removed', removedListener);
+    applicationStore.addEventListener('error', (error) => console.log(error));
+  });
+
+  afterEach(() => {
+    applicationStore.stop();
+  });
+
+  it('registers a new instance', async () => {
+    mockSubject.next({ data: registerWithOneInstance });
+
+    await waitFor(() => {
+      let applications = applicationStore.applications;
+      expect(applications).toHaveLength(1);
+      expect(applications[0].instances).toHaveLength(1);
+    });
+
+    expect(changedListener).toHaveBeenCalled();
+    expect(addedListener).toHaveBeenCalled();
+    expect(updateListener).not.toHaveBeenCalled();
+    expect(removedListener).not.toHaveBeenCalled();
+  });
+
+  it('registers one instance and then another one', async () => {
+    mockSubject.next({ data: registerWithOneInstance });
+    mockSubject.next({ data: registerWithTwoInstances });
+
+    await waitFor(() => {
+      let applications = applicationStore.applications;
+      expect(applications).toHaveLength(1);
+      expect(applications[0].instances).toHaveLength(2);
+    });
+
+    expect(changedListener).toHaveBeenCalled();
+    expect(addedListener).toHaveBeenCalled();
+    expect(updateListener).toHaveBeenCalled();
+    expect(removedListener).not.toHaveBeenCalled();
+  });
+
+  it('deregisters an instance', async () => {
+    mockSubject.next({ data: registerWithTwoInstances });
+    mockSubject.next({ data: registerWithOneInstance });
+
+    await waitFor(() => {
+      let applications = applicationStore.applications;
+      expect(applications).toHaveLength(1);
+      expect(applications[0].instances).toHaveLength(1);
+    });
+
+    expect(changedListener).toHaveBeenCalled();
+    expect(addedListener).toHaveBeenCalled();
+    expect(updateListener).toHaveBeenCalled();
+    expect(removedListener).not.toHaveBeenCalled();
+  });
+
+  it('removes an application', async () => {
+    mockSubject.next({ data: registerWithOneInstance });
+
+    await waitFor(() => {
+      expect(applicationStore.applications).toHaveLength(1);
+    });
+
+    let data = { ...registerWithOneInstance, instances: [] };
+    mockSubject.next({ data: data });
+
+    await waitFor(() => {
+      expect(applicationStore.applications).toHaveLength(0);
+    });
+
+    expect(changedListener).toHaveBeenCalled();
+    expect(addedListener).toHaveBeenCalled();
+    expect(updateListener).not.toHaveBeenCalled();
+    expect(removedListener).toHaveBeenCalled();
+  });
+});
