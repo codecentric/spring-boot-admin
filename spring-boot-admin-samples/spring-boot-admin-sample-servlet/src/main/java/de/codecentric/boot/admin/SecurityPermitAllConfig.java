2371908092dbffa339b7bc5c070584cc4531b500
Stephan Köninger <stephan.koeninger@codecentric.de>
2023-01-20 09:14:14 +0100

    chore: fix checkstyle

--- spring-boot-admin-samples/spring-boot-admin-sample-servlet/src/main/java/de/codecentric/boot/admin/SecurityPermitAllConfig.java
+++ spring-boot-admin-samples/spring-boot-admin-sample-servlet/src/main/java/de/codecentric/boot/admin/SecurityPermitAllConfig.java
@@ -16,11 +16,6 @@
 
 package de.codecentric.boot.admin;
 
-import static org.springframework.http.HttpMethod.DELETE;
-import static org.springframework.http.HttpMethod.POST;
-
-import de.codecentric.boot.admin.server.config.AdminServerProperties;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -30,6 +25,11 @@ import org.springframework.security.web.authentication.www.BasicAuthenticationFi
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.security.web.csrf.CsrfTokenRequestAttributeHandler;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import de.codecentric.boot.admin.server.config.AdminServerProperties;
+
+import static org.springframework.http.HttpMethod.DELETE;
+import static org.springframework.http.HttpMethod.POST;
 
 @Profile("insecure")
 @Configuration(proxyBeanMethods = false)
@@ -44,10 +44,10 @@ public class SecurityPermitAllConfig {
 	@Bean
 	protected SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
-		http.authorizeHttpRequests(authorizeRequest -> authorizeRequest.anyRequest().permitAll());
+		http.authorizeHttpRequests((authorizeRequest) -> authorizeRequest.anyRequest().permitAll());
 
 		http.addFilterAfter(new CustomCsrfFilter(), BasicAuthenticationFilter.class)
-			.csrf(csrf -> csrf.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+			.csrf((csrf) -> csrf.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
 				.csrfTokenRequestHandler(new CsrfTokenRequestAttributeHandler()).ignoringRequestMatchers(
 					new AntPathRequestMatcher(this.adminServer.path("/instances"), POST.toString()),
 					new AntPathRequestMatcher(this.adminServer.path("/instances/*"), DELETE.toString()),
