b3188c41292c6b31edf300d0c7ba1ca0ed8670fa
ulrichschulte <ulrich.schulte@codecentric.de>
2023-02-10 11:13:00 +0100

    WIP - Spring Boot Admin Server GraalVM sample

---
+++ spring-boot-admin-samples/spring-boot-admin-sample-servlet-graalvm/src/main/java/de/codecentric/boot/admin/SpringBootAdminServletApplication.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2014-2020 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.codecentric.boot.admin;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.context.metrics.buffering.BufferingApplicationStartup;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+
+@Configuration(proxyBeanMethods = false)
+@EnableAutoConfiguration
+@EnableAdminServer
+@Lazy(false)
+public class SpringBootAdminServletApplication {
+
+	private static final Logger log = LoggerFactory.getLogger(SpringBootAdminServletApplication.class);
+
+	public static void main(String[] args) {
+		SpringApplication app = new SpringApplication(SpringBootAdminServletApplication.class);
+		app.setApplicationStartup(new BufferingApplicationStartup(1500));
+		app.run(args);
+	}
+
+}
