697bd45243447e99fc7d9ec5ade854f6ad5564d8
Stephan Köninger <stephan.koeninger@codecentric.de>
2023-03-06 22:07:31 +0100

    copy routes.txt file to target directory to include custom routes in HomepageForwardingFilter

--- spring-boot-admin-samples/spring-boot-admin-sample-custom-ui/vite.config.js
+++ spring-boot-admin-samples/spring-boot-admin-sample-custom-ui/vite.config.js
@@ -1,9 +1,20 @@
 import vue from "@vitejs/plugin-vue";
 import path from "path";
 import { defineConfig } from "vite";
+import { viteStaticCopy } from "vite-plugin-static-copy";
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: "src/routes.txt",
+          dest: "./",
+        },
+      ],
+    }),
+  ],
   build: {
     target: "es2015",
     sourcemap: true,
