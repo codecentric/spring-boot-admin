d12bf20cbe1a082aa79180bf9939cb53e806a15b
ulrichschulte <ulrich.schulte@codecentric.de>
2023-03-19 01:15:10 +0100

    Extended RuntimeHints

--- spring-boot-admin-samples/spring-boot-admin-sample-servlet-graalvm/src/main/java/de/codecentric/boot/admin/RuntimeHints.java
+++ spring-boot-admin-samples/spring-boot-admin-sample-servlet-graalvm/src/main/java/de/codecentric/boot/admin/RuntimeHints.java
@@ -125,93 +125,93 @@ public class RuntimeHints implements RuntimeHintsRegistrar {
 		Class<?> queryEndpointStrategyResponse = Class
 			.forName("de.codecentric.boot.admin.server.services.endpoints.QueryIndexEndpointStrategy$Response");
 		Class<?> queryEndpointStrategyResponseEndpointRef = Class.forName(
-			"de.codecentric.boot.admin.server.services.endpoints.QueryIndexEndpointStrategy$Response$EndpointRef");
+				"de.codecentric.boot.admin.server.services.endpoints.QueryIndexEndpointStrategy$Response$EndpointRef");
 
 		hints.reflection()
 			.registerType(queryEndpointStrategyResponse, MemberCategory.INVOKE_PUBLIC_METHODS,
-				MemberCategory.INVOKE_PUBLIC_CONSTRUCTORS)
+					MemberCategory.INVOKE_PUBLIC_CONSTRUCTORS)
 			.registerType(queryEndpointStrategyResponseEndpointRef, MemberCategory.INVOKE_PUBLIC_METHODS,
-				MemberCategory.INVOKE_PUBLIC_CONSTRUCTORS)
+					MemberCategory.INVOKE_PUBLIC_CONSTRUCTORS)
 			.registerType(Application.Builder.class, MemberCategory.INVOKE_PUBLIC_METHODS,
-				MemberCategory.INVOKE_PUBLIC_CONSTRUCTORS)
+					MemberCategory.INVOKE_PUBLIC_CONSTRUCTORS)
 			.registerType(InstanceDeregisteredEvent.class, MemberCategory.INVOKE_PUBLIC_METHODS,
-				MemberCategory.INVOKE_PUBLIC_CONSTRUCTORS)
+					MemberCategory.INVOKE_PUBLIC_CONSTRUCTORS)
 			.registerType(InstanceEndpointsDetectedEvent.class, MemberCategory.INVOKE_PUBLIC_METHODS,
-				MemberCategory.INVOKE_PUBLIC_CONSTRUCTORS)
+					MemberCategory.INVOKE_PUBLIC_CONSTRUCTORS)
 			.registerType(InstanceEvent.class, MemberCategory.INVOKE_PUBLIC_METHODS,
-				MemberCategory.INVOKE_PUBLIC_CONSTRUCTORS)
+					MemberCategory.INVOKE_PUBLIC_CONSTRUCTORS)
 			.registerType(InstanceInfoChangedEvent.class, MemberCategory.INVOKE_PUBLIC_METHODS,
-				MemberCategory.INVOKE_PUBLIC_CONSTRUCTORS)
+					MemberCategory.INVOKE_PUBLIC_CONSTRUCTORS)
 			.registerType(InstanceRegisteredEvent.class, MemberCategory.INVOKE_PUBLIC_METHODS,
-				MemberCategory.INVOKE_PUBLIC_CONSTRUCTORS)
+					MemberCategory.INVOKE_PUBLIC_CONSTRUCTORS)
 			.registerType(InstanceRegistrationUpdatedEvent.class, MemberCategory.INVOKE_PUBLIC_METHODS,
-				MemberCategory.INVOKE_PUBLIC_CONSTRUCTORS)
+					MemberCategory.INVOKE_PUBLIC_CONSTRUCTORS)
 			.registerType(InstanceStatusChangedEvent.class, MemberCategory.INVOKE_PUBLIC_METHODS,
-				MemberCategory.INVOKE_PUBLIC_CONSTRUCTORS)
+					MemberCategory.INVOKE_PUBLIC_CONSTRUCTORS)
 			.registerType(InstanceId.class, MemberCategory.INVOKE_PUBLIC_METHODS,
-				MemberCategory.INVOKE_PUBLIC_CONSTRUCTORS)
+					MemberCategory.INVOKE_PUBLIC_CONSTRUCTORS)
 			.registerType(Application.class, MemberCategory.INVOKE_PUBLIC_METHODS,
-				MemberCategory.INVOKE_PUBLIC_CONSTRUCTORS)
+					MemberCategory.INVOKE_PUBLIC_CONSTRUCTORS)
 			.registerType(Endpoint.class, MemberCategory.INVOKE_PUBLIC_METHODS,
-				MemberCategory.INVOKE_PUBLIC_CONSTRUCTORS)
+					MemberCategory.INVOKE_PUBLIC_CONSTRUCTORS)
 			.registerType(Instance.class, MemberCategory.INVOKE_PUBLIC_METHODS,
-				MemberCategory.INVOKE_PUBLIC_CONSTRUCTORS)
+					MemberCategory.INVOKE_PUBLIC_CONSTRUCTORS)
 			.registerType(InstanceId.class, MemberCategory.INVOKE_PUBLIC_METHODS,
-				MemberCategory.INVOKE_PUBLIC_CONSTRUCTORS)
+					MemberCategory.INVOKE_PUBLIC_CONSTRUCTORS)
 			.registerType(InstanceWebProxy.InstanceResponse.class, MemberCategory.INVOKE_PUBLIC_METHODS,
-				MemberCategory.INVOKE_PUBLIC_CONSTRUCTORS)
+					MemberCategory.INVOKE_PUBLIC_CONSTRUCTORS)
 			.registerType(InstanceWebProxy.ForwardRequest.class, MemberCategory.INVOKE_PUBLIC_METHODS,
-				MemberCategory.INVOKE_PUBLIC_CONSTRUCTORS)
+					MemberCategory.INVOKE_PUBLIC_CONSTRUCTORS)
 			.registerType(AdminServerUiProperties.Palette.class, MemberCategory.INVOKE_PUBLIC_METHODS,
-				MemberCategory.INVOKE_PUBLIC_CONSTRUCTORS)
+					MemberCategory.INVOKE_PUBLIC_CONSTRUCTORS)
 			.registerType(CssColorUtils.class, MemberCategory.INVOKE_PUBLIC_METHODS,
-				MemberCategory.INVOKE_PUBLIC_CONSTRUCTORS)
+					MemberCategory.INVOKE_PUBLIC_CONSTRUCTORS)
 
 			.registerType(BuildVersionMixin.class, MemberCategory.INVOKE_PUBLIC_METHODS,
-				MemberCategory.INVOKE_PUBLIC_CONSTRUCTORS)
+					MemberCategory.INVOKE_PUBLIC_CONSTRUCTORS)
 			.registerType(EndpointMixin.class, MemberCategory.INVOKE_PUBLIC_METHODS,
-				MemberCategory.INVOKE_PUBLIC_CONSTRUCTORS)
+					MemberCategory.INVOKE_PUBLIC_CONSTRUCTORS)
 			.registerType(EndpointsMixin.class, MemberCategory.INVOKE_PUBLIC_METHODS,
-				MemberCategory.INVOKE_PUBLIC_CONSTRUCTORS)
+					MemberCategory.INVOKE_PUBLIC_CONSTRUCTORS)
 			.registerType(InfoMixin.class, MemberCategory.INVOKE_PUBLIC_METHODS,
-				MemberCategory.INVOKE_PUBLIC_CONSTRUCTORS)
+					MemberCategory.INVOKE_PUBLIC_CONSTRUCTORS)
 			.registerType(InstanceDeregisteredEventMixin.class, MemberCategory.INVOKE_PUBLIC_METHODS,
-				MemberCategory.INVOKE_PUBLIC_CONSTRUCTORS)
+					MemberCategory.INVOKE_PUBLIC_CONSTRUCTORS)
 			.registerType(InstanceEndpointsDetectedEventMixin.class, MemberCategory.INVOKE_PUBLIC_METHODS,
-				MemberCategory.INVOKE_PUBLIC_CONSTRUCTORS)
+					MemberCategory.INVOKE_PUBLIC_CONSTRUCTORS)
 			.registerType(InstanceEventMixin.class, MemberCategory.INVOKE_PUBLIC_METHODS,
-				MemberCategory.INVOKE_PUBLIC_CONSTRUCTORS)
+					MemberCategory.INVOKE_PUBLIC_CONSTRUCTORS)
 			.registerType(InstanceIdMixin.class, MemberCategory.INVOKE_PUBLIC_METHODS,
-				MemberCategory.INVOKE_PUBLIC_CONSTRUCTORS)
+					MemberCategory.INVOKE_PUBLIC_CONSTRUCTORS)
 			.registerType(InstanceInfoChangedEventMixin.class, MemberCategory.INVOKE_PUBLIC_METHODS,
-				MemberCategory.INVOKE_PUBLIC_CONSTRUCTORS)
+					MemberCategory.INVOKE_PUBLIC_CONSTRUCTORS)
 			.registerType(InstanceRegisteredEventMixin.class, MemberCategory.INVOKE_PUBLIC_METHODS,
-				MemberCategory.INVOKE_PUBLIC_CONSTRUCTORS)
+					MemberCategory.INVOKE_PUBLIC_CONSTRUCTORS)
 			.registerType(InstanceRegistrationUpdatedEventMixin.class, MemberCategory.INVOKE_PUBLIC_METHODS,
-				MemberCategory.INVOKE_PUBLIC_CONSTRUCTORS)
+					MemberCategory.INVOKE_PUBLIC_CONSTRUCTORS)
 			.registerType(InstanceStatusChangedEventMixin.class, MemberCategory.INVOKE_PUBLIC_METHODS,
-				MemberCategory.INVOKE_PUBLIC_CONSTRUCTORS)
+					MemberCategory.INVOKE_PUBLIC_CONSTRUCTORS)
 			.registerType(StatusInfoMixin.class, MemberCategory.INVOKE_PUBLIC_METHODS,
-				MemberCategory.INVOKE_PUBLIC_CONSTRUCTORS)
+					MemberCategory.INVOKE_PUBLIC_CONSTRUCTORS)
 			.registerType(TagsMixin.class, MemberCategory.INVOKE_PUBLIC_METHODS,
-				MemberCategory.INVOKE_PUBLIC_CONSTRUCTORS)
+					MemberCategory.INVOKE_PUBLIC_CONSTRUCTORS)
 
 			.registerConstructor(queryEndpointStrategyResponse.getConstructors()[0], ExecutableMode.INVOKE)
 			.registerConstructor(
-				queryEndpointStrategyResponseEndpointRef.getDeclaredConstructor(String.class, boolean.class),
-				ExecutableMode.INVOKE)
+					queryEndpointStrategyResponseEndpointRef.getDeclaredConstructor(String.class, boolean.class),
+					ExecutableMode.INVOKE)
 			.registerConstructor(Application.Builder.class.getDeclaredConstructor(), ExecutableMode.INVOKE)
 			.registerMethod(Application.Builder.class.getMethod("build"), ExecutableMode.INVOKE)
 			.registerMethod(Application.class.getMethod("builder"), ExecutableMode.INVOKE)
 			.registerConstructor(Registration.class.getDeclaredConstructor(String.class, String.class, String.class,
-				String.class, String.class, Map.class), ExecutableMode.INVOKE)
+					String.class, String.class, Map.class), ExecutableMode.INVOKE)
 			.registerConstructor(Registration.Builder.class.getDeclaredConstructor(), ExecutableMode.INVOKE)
 			.registerMethod(Registration.Builder.class.getMethod("build"), ExecutableMode.INVOKE)
 			.registerMethod(Registration.class.getMethod("toBuilder"), ExecutableMode.INVOKE)
 			.registerTypes(TypeReference.listOf(StdJdkSerializers.AtomicBooleanSerializer.class,
 					StdJdkSerializers.AtomicIntegerSerializer.class, StdJdkSerializers.AtomicLongSerializer.class,
 					FileSerializer.class, ClassSerializer.class, TokenBufferSerializer.class),
-				TypeHint.builtWith(MemberCategory.INVOKE_PUBLIC_CONSTRUCTORS));
+					TypeHint.builtWith(MemberCategory.INVOKE_PUBLIC_CONSTRUCTORS));
 	}
 
 }
