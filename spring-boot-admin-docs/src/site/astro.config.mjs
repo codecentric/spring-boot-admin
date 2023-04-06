92726a477a24e61758d8c0f0c50029a4954ebe37
Stephan Köninger <stephan.koeninger@codecentric.de>
2023-04-07 00:05:22 +0200

    set basepath

--- spring-boot-admin-docs/src/site/astro.config.mjs
+++ spring-boot-admin-docs/src/site/astro.config.mjs
@@ -3,16 +3,25 @@ import preact from '@astrojs/preact';
 import react from '@astrojs/react';
 
 // https://astro.build/config
-export default defineConfig({
-	integrations: [
-		// Enable Preact to support Preact JSX components.
-		preact(),
-		// Enable React for the Algolia search component.
-		react(),
-	],
-	site: `https://astro.build`,
-	markdown: {
-		syntaxHighlight: 'prism',
-	},
-	outDir: "../../target/generated-docs",
-});
+let config = {
+  integrations: [
+    // Enable Preact to support Preact JSX components.
+    preact(),
+    // Enable React for the Algolia search component.
+    react()
+  ],
+  site: `http://localhost:3000/spring-boot-admin/3.0.3-SNAPSHOT/`,
+  base: `/spring-boot-admin/3.0.3-SNAPSHOT/`,
+  markdown: {
+    syntaxHighlight: 'prism'
+  },
+  outDir: '../../target/generated-docs'
+};
+if (process.env.CI) {
+  config = {
+    ...config,
+    site: `https://codecentric.github.io/spring-boot-admin/${process.env.VERSION}/`,
+    base: `/spring-boot-admin/${process.env.VERSION}/`
+  };
+}
+export default defineConfig(config);
