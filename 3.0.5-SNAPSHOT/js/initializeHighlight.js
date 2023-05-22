3bcbe2af12b80efd02ccd3e71589d405cf3d0d7e
ulischulte <ulrich.schulte@codecentric.de>
2023-05-22 23:34:22 +0000

    Deploying to gh-pages from @ codecentric/spring-boot-admin@14b9f37590d592b5948fc81387c68b0677b2dc70 🚀

---
+++ 3.0.5-SNAPSHOT/js/initializeHighlight.js
@@ -0,0 +1,14 @@
+/**
+ * Licensed under the MIT License (http://www.opensource.org/licenses/mit-license.php)
+ */
+/**
+ * Initializes highlight.js.
+ * 
+ * This file can be overriden to change the initialization.
+ */
+
+/**
+ * Initializes highlight.js when the document loads.
+ */
+// Uses the default initialization
+hljs.initHighlightingOnLoad();
