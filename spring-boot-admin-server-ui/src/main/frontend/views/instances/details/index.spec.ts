c24e85dff2c0358021d71406f395af3996cb738f
Stephan Köninger <stephan.koeninger@codecentric.de>
2023-09-15 17:33:16 +0200

    feat: show health groups in status panel

--- spring-boot-admin-server-ui/src/main/frontend/views/instances/details/index.spec.ts
+++ spring-boot-admin-server-ui/src/main/frontend/views/instances/details/index.spec.ts
@@ -10,51 +10,51 @@ import Application from '@/services/application';
 import { render } from '@/test-utils';
 
 describe('InstanceDetails', () => {
-    describe('Metrics', () => {
-        it('should hide loading spinner, when network call fails', async () => {
-            const application = new Application(applications[0]);
-            const instance = application.instances[0];
+  describe('Metrics', () => {
+    it('should hide loading spinner, when network call fails', async () => {
+      const application = new Application(applications[0]);
+      const instance = application.instances[0];
 
-            server.use(
-                rest.get('/instances/:instanceId/actuator/metrics', (req, res, ctx) => {
-                    return res(ctx.status(404), ctx.json({}));
-                })
-            );
+      server.use(
+        rest.get('/instances/:instanceId/actuator/metrics', (req, res, ctx) => {
+          return res(ctx.status(404), ctx.json({}));
+        })
+      );
 
-            render(DetailsView, {
-                props: {
-                    instance,
-                    application,
-                },
-            });
+      render(DetailsView, {
+        props: {
+          instance,
+          application,
+        },
+      });
 
-            await waitFor(async () => {
-                expect(
-                    await screen.queryByTestId('instance-section-loading-spinner')
-                ).not.toBeInTheDocument();
-            });
-        });
-
-        it('should hide loading spinner, when metrics endpoint is not exposed', async () => {
-            const application = new Application(applications[0]);
-            const instance = application.instances[0];
-
-            instance.hasEndpoint = vi.fn().mockImplementation((endpoint) => {
-                return endpoint !== 'metrics';
-            });
-
-            render(DetailsView, {
-                props: {
-                    instance,
-                    application,
-                },
-            });
-
-            await waitFor(async () => {
-                expect(
-                    await screen.queryByTestId('instance-section-loading-spinner')
-                ).not.toBeInTheDocument();
-            });
-        });
+      await waitFor(async () => {
+        expect(
+          await screen.queryByTestId('instance-section-loading-spinner')
+        ).not.toBeInTheDocument();
+      });
     });
+
+    it('should hide loading spinner, when metrics endpoint is not exposed', async () => {
+      const application = new Application(applications[0]);
+      const instance = application.instances[0];
+
+      instance.hasEndpoint = vi.fn().mockImplementation((endpoint) => {
+        return endpoint !== 'metrics';
+      });
+
+      render(DetailsView, {
+        props: {
+          instance,
+          application,
+        },
+      });
+
+      await waitFor(async () => {
+        expect(
+          await screen.queryByTestId('instance-section-loading-spinner')
+        ).not.toBeInTheDocument();
+      });
+    });
+  });
 });
