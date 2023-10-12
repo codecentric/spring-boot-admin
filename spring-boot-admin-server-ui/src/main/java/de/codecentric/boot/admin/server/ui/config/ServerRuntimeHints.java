b1de1fac6b13368673163c3cfedf3897259f3ff9
ulrichschulte <ulrich.schulte@codecentric.de>
2023-10-12 08:41:41 +0200

    #2807: Fix external ui config for native builds

--- spring-boot-admin-server-ui/src/main/java/de/codecentric/boot/admin/server/ui/config/ServerRuntimeHints.java
+++ spring-boot-admin-server-ui/src/main/java/de/codecentric/boot/admin/server/ui/config/ServerRuntimeHints.java
@@ -114,10 +114,18 @@ public class ServerRuntimeHints implements RuntimeHintsRegistrar {
 					MemberCategory.INVOKE_PUBLIC_CONSTRUCTORS)
 			.registerType(UiController.Settings.class, MemberCategory.INVOKE_PUBLIC_METHODS,
 					MemberCategory.INVOKE_PUBLIC_CONSTRUCTORS)
+			.registerType(UiController.ExternalView.class, MemberCategory.INVOKE_PUBLIC_METHODS,
+				MemberCategory.INVOKE_PUBLIC_CONSTRUCTORS)
 			.registerType(AdminServerUiProperties.UiTheme.class, MemberCategory.INVOKE_PUBLIC_METHODS,
 					MemberCategory.INVOKE_PUBLIC_CONSTRUCTORS)
+			.registerType(AdminServerUiProperties.class, MemberCategory.INVOKE_PUBLIC_METHODS,
+				MemberCategory.INVOKE_PUBLIC_CONSTRUCTORS)
 			.registerType(AdminServerUiProperties.Palette.class, MemberCategory.INVOKE_PUBLIC_METHODS,
 					MemberCategory.INVOKE_PUBLIC_CONSTRUCTORS)
+			.registerType(AdminServerUiProperties.Cache.class, MemberCategory.INVOKE_PUBLIC_METHODS,
+				MemberCategory.INVOKE_PUBLIC_CONSTRUCTORS)
+			.registerType(AdminServerUiProperties.PollTimer.class, MemberCategory.INVOKE_PUBLIC_METHODS,
+				MemberCategory.INVOKE_PUBLIC_CONSTRUCTORS)
 			.registerType(CssColorUtils.class, MemberCategory.INVOKE_PUBLIC_METHODS,
 					MemberCategory.INVOKE_PUBLIC_CONSTRUCTORS)
 			.registerType(InstanceDeregisteredEvent.class, MemberCategory.INVOKE_PUBLIC_METHODS,
