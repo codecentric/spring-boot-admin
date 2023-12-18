08d96160b4dd4bf1b2c12a256ed20f6882c107fd
Stephan Köninger <stephan.koeninger@codecentric.de>
2023-12-18 21:44:52 +0100

    fix: eslint

--- spring-boot-admin-server-ui/src/main/frontend/views/applications/ApplicationListItemAction.spec.ts
+++ spring-boot-admin-server-ui/src/main/frontend/views/applications/ApplicationListItemAction.spec.ts
@@ -17,7 +17,7 @@ import userEvent from '@testing-library/user-event';
 import { screen, waitFor } from '@testing-library/vue';
 import { cloneDeep } from 'lodash-es';
 import { rest } from 'msw';
-import { beforeAll, beforeEach, describe, expect, it, vi } from 'vitest';
+import { beforeEach, describe, expect, it, vi } from 'vitest';
 
 import Application from '../../services/application';
 
