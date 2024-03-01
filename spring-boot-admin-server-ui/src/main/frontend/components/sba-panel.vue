70ec23cfc2e67e8d49b742a4e1ca0e61ebb157c5
ulrichschulte <ulrich.schulte@codecentric.de>
2024-03-01 15:08:33 +0100

    #3126: Show version for application group

--- spring-boot-admin-server-ui/src/main/frontend/components/sba-panel.vue
+++ spring-boot-admin-server-ui/src/main/frontend/components/sba-panel.vue
@@ -38,9 +38,11 @@
           <slot v-if="'title' in $slots" name="title" />
         </button>
       </h3>
+
       <div>
         <slot v-if="'version' in $slots" name="version" />
       </div>
+
       <div>
         <slot v-if="'actions' in $slots" name="actions" />
         <sba-icon-button
