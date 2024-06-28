78f2133b185146ca808d38d385e507d3fed22076
ulrichschulte <ulrich.schulte@codecentric.de>
2024-06-28 13:27:47 +0200

    feat: Added conditional Bean details

---
+++ spring-boot-admin-server-ui/src/main/frontend/views/instances/conditions/conditions-list-details.vue
@@ -0,0 +1,42 @@
+<template>
+  <table>
+    <colgroup>
+      <col class="text-gray-500" />
+      <col class="py-3" />
+    </colgroup>
+    <tbody>
+      <!-- Condition -->
+      <tr v-if="condition.condition">
+        <th class="label" v-text="$t('instances.conditions.condition')" />
+        <td class="value" v-text="condition.condition" />
+      </tr>
+
+      <!-- Message -->
+      <tr v-if="condition.message">
+        <th class="label" v-text="$t('instances.conditions.message')" />
+        <td class="value" v-text="condition.message" />
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<script>
+export default {
+  name: 'ConditionsListDetails',
+  props: {
+    condition: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style>
+.label {
+  @apply text-gray-500 text-sm text-right px-2 align-top w-32;
+}
+.value {
+  @apply text-sm break-all;
+}
+</style>
