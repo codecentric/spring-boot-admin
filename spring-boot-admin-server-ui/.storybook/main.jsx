4f313f0f936567134329db91ccbcae6100836af5
ulrichschulte <ulrich.schulte@codecentric.de>
2025-08-22 11:23:19 +0200

    fix storybook 9

--- spring-boot-admin-server-ui/.storybook/main.jsx
+++ spring-boot-admin-server-ui/.storybook/main.jsx
@@ -2,16 +2,10 @@ const { mergeConfig } = require('vite');
 const path = require('path');
 const frontend = path.resolve(__dirname, '../src/main/frontend/');
 module.exports = {
-  stories: [
-    {
-      directory: frontend,
-    },
-  ],
-  addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@storybook/addon-mdx-gfm',
-  ],
+  stories: ['../src/main/frontend/**/*.stories.@(js|jsx|ts|tsx|mdx)'],
+
+  addons: ['@storybook/addon-links', '@storybook/addon-docs'],
+
   framework: {
     name: '@storybook/vue3-vite',
     options: {},
---
+++ spring-boot-admin-server-ui/src/main/frontend/index.stories.js
@@ -0,0 +1,20 @@
+import { DocsContainer } from '@storybook/addon-docs/blocks';
+
+const ExampleContainer = ({ children, ...props }) => {
+return <DocsContainer {...props}><div className="prose">{children}</div></DocsContainer>;
+};
+
+export default {
+  parameters: {
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/,
+      },
+    },
+    docs: {
+      container: ExampleContainer,
+    },
+  },
+};
+
