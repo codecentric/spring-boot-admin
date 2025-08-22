9c96d269d04a878398940baf15ede81b5efbc5c3
Ulrich Schulte <ulrich.schulte@codecentric.de>
2025-08-22 14:48:48 +0200

    Feat/fix storybook (#4590)
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>

--- spring-boot-admin-server-ui/.storybook/main.js
+++ spring-boot-admin-server-ui/.storybook/main.js
@@ -2,20 +2,14 @@ const { mergeConfig } = require('vite');
 const path = require('path');
 const frontend = path.resolve(__dirname, '../src/main/frontend/');
 module.exports = {
-  stories: [
-    {
-      directory: frontend,
-    },
-  ],
-  addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@storybook/addon-mdx-gfm',
-  ],
+  stories: ['../src/main/frontend/**/*.stories.@(js|jsx|ts|tsx|mdx)'],
+  addons: ['@storybook/addon-links', '@storybook/addon-docs'],
+
   framework: {
     name: '@storybook/vue3-vite',
     options: {},
   },
+
   async viteFinal(config) {
     config.plugins = config.plugins.filter((p) => p.name !== 'vue-docgen');
     return mergeConfig(config, {
@@ -26,8 +20,5 @@ module.exports = {
         extensions: ['.vue', '.js', '.json'],
       },
     });
-  },
-  docs: {
-    autodocs: true,
-  },
+  }
 };
