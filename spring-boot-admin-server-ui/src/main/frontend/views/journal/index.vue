e047aa74978a936757bc17b522f76d66b6c28906
Stephan Köninger <stephan.koeninger@codecentric.de>
2025-09-21 11:42:41 +0200

    fix: refactor InstanceShell component and improve event handling

--- spring-boot-admin-server-ui/src/main/frontend/views/journal/index.vue
+++ spring-boot-admin-server-ui/src/main/frontend/views/journal/index.vue
@@ -46,7 +46,10 @@ import { useDateTimeFormatter } from '@/composables/useDateTimeFormatter';
 import subscribing from '@/mixins/subscribing';
 import Instance from '@/services/instance';
 import { compareBy } from '@/utils/collections';
-import { InstanceEvent } from '@/views/journal/InstanceEvent';
+import {
+  InstanceEvent,
+  InstanceEventType,
+} from '@/views/journal/InstanceEvent';
 import JournalTable from '@/views/journal/JournalTable.vue';
 
 export default {
@@ -79,8 +82,8 @@ export default {
       return this.events
         .filter(
           (event) =>
-            event.type === InstanceEvent.REGISTERED ||
-            event.type === InstanceEvent.REGISTRATION_UPDATED,
+            event.type === InstanceEventType.REGISTERED ||
+            event.type === InstanceEventType.REGISTRATION_UPDATED,
         )
         .sort((a, b) => b.timestamp - a.timestamp)
         .reduceRight((names, event) => {
