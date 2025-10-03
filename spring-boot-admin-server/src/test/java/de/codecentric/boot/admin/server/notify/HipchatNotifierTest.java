cb6cd3e104997aa03f03d210458c4c18198fd83f
Andreas Höhmann <andreas.hoehmann@siemens.com>
2025-10-03 12:53:34 +0200

    wip: move on to springboot 4.0.0-M3 and springcloud 2025.1.0-M2
    - cleaned also all "invalid" code-formattings from previous commits
    
    - one big problem in spring-boot-admin-server
    
    jackson's databind module 3.0.0-rc9 was included into spring which
    conflicts here and there with the old jackson stuff?!

--- spring-boot-admin-server/src/test/java/de/codecentric/boot/admin/server/notify/HipchatNotifierTest.java
+++ spring-boot-admin-server/src/test/java/de/codecentric/boot/admin/server/notify/HipchatNotifierTest.java
@@ -56,7 +56,7 @@ class HipchatNotifierTest {
 
 	@BeforeEach
 	void setUp() {
-		final InstanceRepository repository = mock(InstanceRepository.class);
+		InstanceRepository repository = mock(InstanceRepository.class);
 		when(repository.find(instance.getId())).thenReturn(Mono.just(instance));
 
 		restTemplate = mock(RestTemplate.class);
@@ -70,7 +70,7 @@ class HipchatNotifierTest {
 	@Test
 	void test_onApplicationEvent_resolve() {
 		@SuppressWarnings("unchecked")
-		final ArgumentCaptor<HttpEntity<Map<String, Object>>> httpRequest = ArgumentCaptor
+		ArgumentCaptor<HttpEntity<Map<String, Object>>> httpRequest = ArgumentCaptor
 			.forClass((Class<HttpEntity<Map<String, Object>>>) (Class<?>) HttpEntity.class);
 
 		when(restTemplate.postForEntity(isA(String.class), httpRequest.capture(), eq(Void.class)))
@@ -88,7 +88,7 @@ class HipchatNotifierTest {
 		assertThat(httpRequest.getValue().getHeaders().asMultiValueMap()).containsEntry("Content-Type",
 			.containsAll(Collections.singletonList("application/json"));
 
-		final Map<String, Object> body = httpRequest.getValue().getBody();
+		Map<String, Object> body = httpRequest.getValue().getBody();
 		assertThat(body).containsEntry("color", "green");
 		assertThat(body).containsEntry("message", "<strong>App</strong>/-id- is <strong>UP</strong>");
 		assertThat(body).containsEntry("notify", Boolean.TRUE);
@@ -98,10 +98,10 @@ class HipchatNotifierTest {
 
 	@Test
 	void test_onApplicationEvent_trigger() {
-		final StatusInfo infoDown = StatusInfo.ofDown();
+		StatusInfo infoDown = StatusInfo.ofDown();
 
 		@SuppressWarnings("unchecked")
-		final ArgumentCaptor<HttpEntity<Map<String, Object>>> httpRequest = ArgumentCaptor
+		ArgumentCaptor<HttpEntity<Map<String, Object>>> httpRequest = ArgumentCaptor
 			.forClass((Class<HttpEntity<Map<String, Object>>>) (Class<?>) HttpEntity.class);
 
 		when(restTemplate.postForEntity(isA(String.class), httpRequest.capture(), eq(Void.class)))
@@ -117,7 +117,7 @@ class HipchatNotifierTest {
 
 		assertThat(httpRequest.getValue().getHeaders().toSingleValueMap()).containsEntry("Content-Type",
 				"application/json");
-		final Map<String, Object> body = httpRequest.getValue().getBody();
+		Map<String, Object> body = httpRequest.getValue().getBody();
 		assertThat(body).containsEntry("color", "red");
 		assertThat(body).containsEntry("message", "<strong>App</strong>/-id- is <strong>DOWN</strong>");
 		assertThat(body).containsEntry("notify", Boolean.TRUE);
