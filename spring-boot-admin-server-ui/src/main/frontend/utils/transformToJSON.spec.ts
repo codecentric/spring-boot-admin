f7f821f30ac91aa11290df93e7377f7bb20acb70
Stephan Köninger <stephan.koeninger@codecentric.de>
2025-11-16 16:37:42 +0100

    fix: enhance metadata parsing with LAX mode for conflict resolution (#4843)

--- spring-boot-admin-server-ui/src/main/frontend/utils/transformToJSON.spec.ts
+++ spring-boot-admin-server-ui/src/main/frontend/utils/transformToJSON.spec.ts
@@ -11,22 +11,37 @@ const input = {
 };
 
 describe('transformToNestedPojo', () => {
-  const output = transformToJSON(input);
-
   it('transforms simple key', () => {
+    const output = transformToJSON(input);
+
     expect(output['service-url']).toEqual('http://localhost:8080');
   });
 
   it('transforms nested key', () => {
+    const output = transformToJSON(input);
+
     expect(output.tags.environment).toEqual('test');
   });
 
   it('transforms array', () => {
+    const output = transformToJSON(input);
+
     expect(output.sidebar.links).toHaveLength(1);
     expect(output.sidebar.links[0]).toEqual({
       iframe: 'true',
       label: '🏠 Home',
       url: 'https://codecentric.de',
     });
+  });
+
+  it('handles conflict when property is both string and object', () => {
+    const conflictInput = {
+      'my-service-headless.kubernetes': 'namespace',
+      'my-service-headless': 'some-value',
+    };
+
+    const output = transformToJSON(conflictInput, 'LAX');
+    expect(output['my-service-headless']['__']).toEqual('some-value');
+    expect(output['my-service-headless']['kubernetes']).toEqual('namespace');
   });
 });
