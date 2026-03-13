c0a6ef1625315643cf96e91585c65d5bb0a821a1
Stephan Köninger <stephan.koeninger@codecentric.de>
2026-03-13 09:53:28 +0100

    chore: linter

--- spring-boot-admin-server-ui/src/main/frontend/views/instances/sbomdependencytrees/index.spec.ts
+++ spring-boot-admin-server-ui/src/main/frontend/views/instances/sbomdependencytrees/index.spec.ts
@@ -11,7 +11,7 @@ type Deferred<T> = {
   reject: (reason?: unknown) => void;
 };
 
-const deferred = <T,>(): Deferred<T> => {
+const deferred = <T>(): Deferred<T> => {
   let resolve!: (value: T) => void;
   let reject!: (reason?: unknown) => void;
   const promise = new Promise<T>((res, rej) => {
