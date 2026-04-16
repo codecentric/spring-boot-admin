ceada3152d1b1f2fd788065a49e99e4d5ad89047
Harshit <harshitsingh64053@gmail.com>
2026-04-16 20:01:04 +0530

    fix: sidebar group should toggle open/close on click

--- spring-boot-admin-server-ui/src/main/frontend/views/instances/shell/sidebar.vue
+++ spring-boot-admin-server-ui/src/main/frontend/views/instances/shell/sidebar.vue
@@ -20,7 +20,6 @@
     class="h-full flex flex-col bg-white/80 border-r border-gray-200 backdrop-filter backdrop-blur-lg z-40 w-10 md:w-60 transition-all left-0 pb-14 fixed"
   >
     <ul class="relative px-1 py-1 overflow-y-auto">
-
       <!-- Instance info block -->
       <li class="relative mb-1 hidden md:block">
         <router-link
@@ -38,14 +37,14 @@
         </router-link>
       </li>
 
-      <!-- Mobile toggle -->
+      <!-- sm: button toggle navigation -->
       <li class="block md:hidden mb-1">
         <a class="navbar-link navbar-link__group" @click.stop="toggleSidebar">
           <font-awesome-icon :icon="['fas', 'bars']" />
         </a>
       </li>
 
-      <!-- NAV -->
+      <!-- The actual nav -->
       <li
         v-for="group in groups"
         :key="group.name"
@@ -62,7 +61,6 @@
           @click.prevent="toggleGroup(group.id)"
         >
           <span v-html="group.icon" />
-
           <span
             v-text="
               hasMultipleViews(group)
@@ -70,14 +68,19 @@
                 : $t(group.views[0].label)
             "
           />
-
           <svg
             v-if="hasMultipleViews(group)"
             :class="{
-              '-rotate-90': !(openGroup === group.id || isActiveGroup(group)),
+              '-rotate-90': !isActiveGroup(group),
+              '': isActiveGroup(group),
             }"
+            aria-hidden="true"
             class="h-3 ml-auto hidden md:block transition"
+            data-prefix="fas"
+            focusable="false"
+            role="img"
             viewBox="0 0 448 512"
+            xmlns="http://www.w3.org/2000/svg"
           >
             <path
               d="M207.029 381.476L12.686 187.132c-9.373-9.373-9.373-24.569 0-33.941l22.667-22.667c9.357-9.357 24.522-9.375 33.901-.04L224 284.505l154.745-154.021c9.379-9.335 24.544-9.317 33.901.04l22.667 22.667c9.373 9.373 9.373 24.569 0 33.941L240.971 381.476c-9.373 9.372-24.569 9.372-33.942 0z"
@@ -86,7 +89,7 @@
           </svg>
         </router-link>
 
-        <!-- SUBMENU -->
+        <!-- Le subnav -->
         <ul
           v-if="hasMultipleViews(group) && (openGroup === group.id || isActiveGroup(group))"
           :class="{ 'hidden md:block': !sidebarOpen }"
@@ -95,11 +98,13 @@
           <li
             v-for="view in group.views"
             :key="view.name"
+            :data-sba-view="view.name"
           >
             <router-link
               :to="{ name: view.name, params: { instanceId: instance.id } }"
               active-class="navbar-link__active"
               class="navbar-link navbar-link__group_item"
+              exact-active-class=""
             >
               <component :is="view.handle" />
             </router-link>
@@ -107,47 +112,139 @@
         </ul>
       </li>
 
+      <template v-if="customLinksFromMetadata?.length > 0">
+        <Divider align="center" class="my-2!">
+          <small class="bold">
+            {{ $t('sidebar.custom-link.title') }}
+          </small>
+        </Divider>
+
+        <li>
+          <ul
+            :class="{ 'hidden md:block': !sidebarOpen }"
+            class="relative block"
+          >
+            <li
+              v-for="view in customLinksFromMetadata"
+              :key="view.name"
+              :data-sba-view="view.name"
+            >
+              <router-link
+                v-if="view.iframe"
+                :to="{
+                  name: 'instances/custom-link-view',
+                  params: { instanceId: instance.id, url: view.href },
+                }"
+                active-class="navbar-link__active"
+                class="navbar-link navbar-link--custom navbar-link__group_item"
+                exact-active-class=""
+              >
+                <component :is="view.handle" />
+              </router-link>
+              <a
+                v-else-if="view.href"
+                :href="view.href"
+                class="navbar-link navbar-link--custom navbar-link__group_item"
+                target="_blank"
+                rel="noopener"
+              >
+                <component :is="view.handle" />
+                <font-awesome-icon
+                  :icon="faArrowUpRightFromSquare"
+                  class="w-2 ml-1"
+                />
+              </a>
+            </li>
+          </ul>
+        </li>
+      </template>
     </ul>
   </aside>
 </template>
 
 <script lang="ts" setup>
+import { faArrowUpRightFromSquare } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { Divider } from 'primevue';
 import { computed, h, ref, toRaw, watch } from 'vue';
 import { useI18n } from 'vue-i18n';
 import { useRoute } from 'vue-router';
+
+import Application from '@/services/application';
+import Instance from '@/services/instance';
+import { compareBy } from '@/utils/collections';
 import { VIEW_GROUP, VIEW_GROUP_ICON } from '@/views/ViewGroup';
 
-const props = defineProps<any>();
+const props = defineProps<{
+  views: any[];
+  instance: Instance;
+  application: Application;
+}>();
 
 const { t } = useI18n();
 const route = useRoute();
-
 const sidebarOpen = ref(false);
-const openGroup = ref<string | null>(null); 
+const openGroup = ref<string | null>(null);
 
-function toggleGroup(groupId: string) {
-  openGroup.value = openGroup.value === groupId ? null : groupId;
-}
-
-watch(() => route.fullPath, () => {
-  sidebarOpen.value = false;
-  openGroup.value = null; 
+const customLinksFromMetadata = computed(() => {
+  const newVar = props.instance.metadataParsed?.sidebar?.links || [];
+  return newVar.map((view) => {
+    return {
+      handle: () => h('span', { innerHTML: view.label }),
+      href: view.url,
+      iframe: view.iframe || false,
+      order: Number.MAX_SAFE_INTEGER,
+    } satisfies SbaView;
+  });
 });
+
+const enabledViews = computed(() => {
+  if (!props.instance) {
+    return [];
+  }
+  return [...props.views]
+    .filter(
+      (view) =>
+        typeof view.isEnabled === 'undefined' ||
+        view.isEnabled({ instance: props.instance }),
+    )
+    .sort(compareBy((v: any) => v.order));
+});
+
+const groups = computed(() => {
+  const groups = new Map();
+
+  [...enabledViews.value].forEach((view: SbaView) => {
+    const groupName = view.group;
+    const group = groups.get(groupName) || {
+      id: groupName,
+      order: Number.MAX_SAFE_INTEGER,
+      views: [],
+    };
+    groups.set(groupName, {
+      ...group,
+      order: Math.min(group.order, view.order),
+      icon: VIEW_GROUP_ICON[groupName],
+      views: [...group.views, view],
+    });
+  });
+  return Array.from(groups.values());
+});
+
+watch(
+  () => route.fullPath,
+  () => {
+    sidebarOpen.value = false;
+    openGroup.value = null;
+  },
+);
 
 function toggleSidebar() {
   sidebarOpen.value = !sidebarOpen.value;
 }
 
-function isActiveGroup(group: any) {
-  if (group.id === VIEW_GROUP.CUSTOM_LINK) return true;
-
-  return group.views.some(
-    (view: any) => toRaw(view) === route.meta.view,
-  );
-}
-
-function hasMultipleViews(group: any) {
-  return group.views.length > 1;
+function toggleGroup(groupId: string) {
+  openGroup.value = openGroup.value === groupId ? null : groupId;
 }
 
 function getGroupTitle(groupId: string) {
@@ -156,23 +253,50 @@ function getGroupTitle(groupId: string) {
   return key === translated ? groupId : translated;
 }
 
-const groups = computed(() => {
-  const groups = new Map();
+function isActiveGroup(group: any) {
+  if (group.id === VIEW_GROUP.CUSTOM_LINK) {
+    return true;
+  }
 
-  props.views.forEach((view: any) => {
-    const groupName = view.group;
-    const group = groups.get(groupName) || {
-      id: groupName,
-      views: [],
-    };
+  const result = group.views.some(
+    (view: any) => toRaw(view) === route.meta.view,
+  );
+  return result;
+}
 
-    groups.set(groupName, {
-      ...group,
-      icon: VIEW_GROUP_ICON[groupName],
-      views: [...group.views, view],
-    });
-  });
-
-  return Array.from(groups.values());
-});
+function hasMultipleViews(group: any) {
+  return group.views.length > 1;
+}
 </script>
+
+<style scoped>
+@reference "../../../index.css";
+
+.instance-info-block {
+  @apply bg-sba-50/40 text-sba-900 flex items-center text-sm py-4 px-6 text-left overflow-hidden text-ellipsis rounded transition duration-300 ease-in-out cursor-pointer;
+}
+
+a.navbar-link {
+  @apply cursor-pointer;
+}
+.navbar-link {
+  @apply bg-sba-50/40 duration-300 ease-in-out flex  items-center overflow-hidden py-4 rounded text-sm transition whitespace-nowrap;
+  @apply text-gray-700;
+}
+.navbar-link--custom {
+  @apply !px-6;
+}
+
+.navbar-link:hover,
+.navbar-link__active {
+  @apply bg-sba-50/80 text-sba-900;
+}
+
+.navbar-link__group_item {
+  @apply h-6 mb-1 mt-1 pl-12 pr-6;
+}
+
+.navbar-link__group {
+  @apply h-12 px-2 md:px-6;
+}
+</style>
