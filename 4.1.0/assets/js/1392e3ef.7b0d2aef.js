0aa7373c648c773909fc996e711354beaab10f1f
andreasfritz <andreasfritz@users.noreply.github.com>
2026-06-12 06:57:28 +0000

    Deploying to gh-pages from @ codecentric/spring-boot-admin@8517b83deaa17345105b5b75762f1227ad0eda25 🚀

---
+++ 4.1.0/assets/js/1392e3ef.7b0d2aef.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunksite=self.webpackChunksite||[]).push([[6195],{5982:e=>{e.exports=JSON.parse('{"metadata":{"permalink":"/4.1.0/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":0,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
